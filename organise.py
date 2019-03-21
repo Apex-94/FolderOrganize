337c6bc94e5ce363e9aab703500277499e0dffdd
Apex-94 <43545020+Apex-94@users.noreply.github.com>
2019-03-21 22:59:03 +0530

    Added Major file types in Dictionary
    eg "IMAGES"(key): [".jpeg", ".jpg", ".tiff", ".gif", ".bmp", ".png", ".bpg", "svg", 
                   ".heif", ".psd"]

---
+++ organise.py
@@ -0,0 +1,23 @@
+import os 
+from pathlib
+dir= { 
+    "HTML": [".html5", ".html", ".htm", ".xhtml"], 
+    "VIDEOS": [".avi", ".flv", ".wmv", ".mov", ".mp4", ".webm", ".vob", ".mng", 
+               ".qt", ".mpg", ".mpeg", ".3gp"], 
+    "IMAGES": [".jpeg", ".jpg", ".tiff", ".gif", ".bmp", ".png", ".bpg", "svg", 
+               ".heif", ".psd"], 
+    "DOCUMENTS": [".oxps", ".epub", ".pages", ".docx", ".doc", ".fdf", ".ods", 
+                  ".odt", ".pwi", ".xsn", ".xps", ".dotx", ".docm", ".dox", 
+                  ".rvg", ".rtf", ".rtfd", ".wpd", ".xls", ".xlsx", ".ppt", 
+                  "pptx"], 
+    "ARCHIVES": [".7z",".dmg", ".rar", ".xar", ".zip"], 
+    "AUDIO": [".aac", ".aa", ".aac", ".dvf", ".m4a", ".m4b", ".m4p", ".mp3", 
+              ".msv", "ogg", "oga", ".raw", ".vox", ".wav", ".wma"], 
+    "PLAINTEXT": [".txt", ".in", ".out"], 
+    "PDF": [".pdf"], 
+    "PYTHON": [".py"], 
+    "XML": [".xml"], 
+    "EXE": [".exe"], 
+    "SHELL": [".sh"] 
+  
+} 
