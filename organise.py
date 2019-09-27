7e0c20ab1086aae30269db9cd9163ff8f235f0ba
iotarepeat <dhruvam15@gmail.com>
2019-09-27 20:13:08 +0530

    The program actually does something :P

--- organise.py
+++ organise.py
@@ -1,23 +1,87 @@
-import os 
-from pathlib
-dir= { 
-    "HTML": [".html5", ".html", ".htm", ".xhtml"], 
-    "VIDEOS": [".avi", ".flv", ".wmv", ".mov", ".mp4", ".webm", ".vob", ".mng", 
-               ".qt", ".mpg", ".mpeg", ".3gp"], 
-    "IMAGES": [".jpeg", ".jpg", ".tiff", ".gif", ".bmp", ".png", ".bpg", "svg", 
-               ".heif", ".psd"], 
-    "DOCUMENTS": [".oxps", ".epub", ".pages", ".docx", ".doc", ".fdf", ".ods", 
-                  ".odt", ".pwi", ".xsn", ".xps", ".dotx", ".docm", ".dox", 
-                  ".rvg", ".rtf", ".rtfd", ".wpd", ".xls", ".xlsx", ".ppt", 
-                  "pptx"], 
-    "ARCHIVES": [".7z",".dmg", ".rar", ".xar", ".zip"], 
-    "AUDIO": [".aac", ".aa", ".aac", ".dvf", ".m4a", ".m4b", ".m4p", ".mp3", 
-              ".msv", "ogg", "oga", ".raw", ".vox", ".wav", ".wma"], 
-    "PLAINTEXT": [".txt", ".in", ".out"], 
-    "PDF": [".pdf"], 
-    "PYTHON": [".py"], 
-    "XML": [".xml"], 
-    "EXE": [".exe"], 
-    "SHELL": [".sh"] 
-  
-} 
+import os
+
+dir_dict = {
+    "HTML": [".html5", ".html", ".htm", ".xhtml"],
+    "VIDEOS": [
+        ".avi",
+        ".flv",
+        ".wmv",
+        ".mov",
+        ".mp4",
+        ".webm",
+        ".vob",
+        ".mng",
+        ".qt",
+        ".mpg",
+        ".mpeg",
+        ".3gp",
+    ],
+    "IMAGES": [
+        ".jpeg",
+        ".jpg",
+        ".tiff",
+        ".gif",
+        ".bmp",
+        ".png",
+        ".bpg",
+        "svg",
+        ".heif",
+        ".psd",
+    ],
+    "DOCUMENTS": [
+        ".oxps",
+        ".epub",
+        ".pages",
+        ".docx",
+        ".doc",
+        ".fdf",
+        ".ods",
+        ".odt",
+        ".pwi",
+        ".xsn",
+        ".xps",
+        ".dotx",
+        ".docm",
+        ".dox",
+        ".rvg",
+        ".rtf",
+        ".rtfd",
+        ".wpd",
+        ".xls",
+        ".xlsx",
+        ".ppt",
+        "pptx",
+    ],
+    "ARCHIVES": [".7z", ".dmg", ".rar", ".xar", ".zip"],
+    "AUDIO": [
+        ".aac",
+        ".aa",
+        ".aac",
+        ".dvf",
+        ".m4a",
+        ".m4b",
+        ".m4p",
+        ".mp3",
+        ".msv",
+        "ogg",
+        "oga",
+        ".raw",
+        ".vox",
+        ".wav",
+        ".wma",
+    ],
+    "PLAINTEXT": [".txt", ".in", ".out"],
+    "PDF": [".pdf"],
+    "PYTHON": [".py"],
+    "XML": [".xml"],
+    "EXE": [".exe"],
+    "SHELL": [".sh"],
+}
+for files in os.listdir():
+    if "." in files:
+        name, *_, ext = files.split(".")
+        for k, v in dir_dict.items():
+            if "." + ext in v:
+                if not os.path.exists(k):
+                    os.mkdir(k)
+                os.rename(files, os.path.join(k, files))
